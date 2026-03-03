6ad6de3a2507c3703d1ce481f0ca505e613d42eb
wydefa236-gif <wydefa236@gmail.com>
2026-03-03 13:35:01 +0700

    Update default.sh
    SS

--- default.sh
+++ default.sh
@@ -17,7 +17,7 @@ PIP_PACKAGES=(
 
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
-    #"https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/slahiri/ComfyUI-Workflow-Models-Downloader"
 )
 
 WORKFLOWS=(
